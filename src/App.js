b5d48c5abb99c91c13d3f6e871f724f4507121da
giorgio <giorgio.programmer@gmail.com>
2023-12-30 21:41:39 +0100

    a

--- src/App.js
+++ src/App.js
@@ -3,69 +3,69 @@ import Category from './components/Category';
 import './App.css';
 
 const App = () => {
- const [categories, setCategories] = useState([]);
- const [newCategoryTitle, setNewCategoryTitle] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [newCategoryTitle, setNewCategoryTitle] = useState('');
 
- useEffect(() => {
- const categoriesFromStorage = localStorage.getItem('categories');
- if (categoriesFromStorage) {
- setCategories(JSON.parse(categoriesFromStorage));
- }
- }, []);
+  useEffect(() => {
+  const categoriesFromStorage = localStorage.getItem('categories');
+  if (categoriesFromStorage) {
+  setCategories(JSON.parse(categoriesFromStorage));
+  }
+  }, []);
 
- useEffect(() => {
- localStorage.setItem('categories', JSON.stringify(categories));
- }, [categories]);
+  useEffect(() => {
+  localStorage.setItem('categories', JSON.stringify(categories));
+  }, [categories]);
 
- const addCategory = () => {
- if (newCategoryTitle !== '') {
- setCategories([...categories, { id: Date.now(), title: newCategoryTitle, tasks: [] }]);
- setNewCategoryTitle('');
- }
- };
+  const addCategory = () => {
+  if (newCategoryTitle !== '') {
+  setCategories([...categories, { id: Date.now(), title: newCategoryTitle, tasks: [] }]);
+  setNewCategoryTitle('');
+  }
+  };
 
- const deleteCategory = (categoryId) => {
- setCategories(categories.filter((category) => category.id !== categoryId));
- };
+  const deleteCategory = (categoryId) => {
+  setCategories(categories.filter((category) => category.id !== categoryId));
+  };
 
- const addTask = (categoryId, taskTitle) => {
- setCategories(
- categories.map((category) => {
-   if (category.id === categoryId) {
-     return { ...category, tasks: [...category.tasks, { id: Date.now(), title: taskTitle, completed: false }] };
-   } else {
-     return category;
-   }
- })
- );
- };
+  const addTask = (categoryId, taskTitle) => {
+  setCategories(
+  categories.map((category) => {
+    if (category.id === categoryId) {
+      return { ...category, tasks: [...category.tasks, { id: Date.now(), title: taskTitle, completed: false }] };
+    } else {
+      return category;
+    }
+  })
+  );
+  };
 
- const deleteTask = (categoryId, taskId) => {
- setCategories(
- categories.map((category) => {
-   if (category.id === categoryId) {
-     return { ...category, tasks: category.tasks.filter((task) => task.id !== taskId) };
-   } else {
-     return category;
-   }
- })
- );
- };
+  const deleteTask = (categoryId, taskId) => {
+  setCategories(
+  categories.map((category) => {
+    if (category.id === categoryId) {
+      return { ...category, tasks: category.tasks.filter((task) => task.id !== taskId) };
+    } else {
+      return category;
+    }
+  })
+  );
+  };
 
- return (
- <div className="app">
- {categories.map((category) => (
-   <Category key={category.id} category={category} tasks={category.tasks} addTask={addTask} deleteTask={deleteTask} deleteCategory={deleteCategory} />
- ))}
- <input 
-   type="text" 
-   placeholder="Aggiungi una categoria..." 
-   value={newCategoryTitle} 
-   onChange={(e) => setNewCategoryTitle(e.target.value)}
- />
- <button onClick={addCategory}>Aggiungi Categoria</button>
- </div>
- );
+  return (
+  <div className="app">
+  {categories.map((category) => (
+    <Category key={category.id} category={category} tasks={category.tasks} addTask={addTask} deleteTask={deleteTask} deleteCategory={deleteCategory} />
+  ))}
+  <input 
+    type="text" 
+    placeholder="Aggiungi una categoria..." 
+    value={newCategoryTitle} 
+    onChange={(e) => setNewCategoryTitle(e.target.value)}
+  />
+  <button onClick={addCategory}>Aggiungi Categoria</button>
+  </div>
+  );
 };
 
 export default App;
